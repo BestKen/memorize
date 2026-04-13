d8046c53d461fe0b8ab37065aab11eff8aa5ea28
Ken <ken@ltc.tw>
2026-04-13 19:03:29 +0800

    feat: match

--- memorize/EmojiMemoryGame.swift
+++ memorize/EmojiMemoryGame.swift
@@ -7,14 +7,29 @@
 
 import Foundation
 
-func createCardContent(index: Int) -> String {
-    return "A"
-}
-
+@Observable
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4, createCardContent: createCardContent )
+    
+    private static var emojis = ["鼠", "牛", "虎","兔", "🚚", "✈️", "🚡", "🛰", "🚅", "🚗", "🚕", "🚌", "🚎", "🚓", "🚑", "🚒", "🛵", "🚜", "🛴", "🏍", "🛺", "🚨", "🚃", "🛳", "🛥", "🚤", "⛵️", "🛶"]
+
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4, createCardContent: {index in EmojiMemoryGame.emojis[index] })
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+    
     
     var cards: [MemoryGame<String>.Card] {
         model.cards
+    }
+    
+    // MARK: - intent
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+    
+    func shuffle() {
+        model.shuffle()
     }
 }
